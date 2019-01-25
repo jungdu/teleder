c82a410c502c9ef76e9407caca9664bc2adbbecb
jungdu <jungdujang@gmail.com>
2019-01-26 00:06:02 +0900

    Define graphql schema and connect graphql to express

---
+++ server/datafeed/datafeed.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const data = require('./goodreads_data');
+const Quote = require('../models/quote');
+
+mongoose.connect('mongodb://localhost/teleder', { useNewUrlParser: true });
+mongoose.connection
+    .once('open', () =>{
+      Quote.insertMany(data)
+        .then(result => {
+          console.log(result);
+        });
+    })
+    .on('error', error => console.log('Error connecting to MongoLab:', error));
+  
---
+++ server/datafeed/goodreads_data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    content:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    from: "Albert Einstein"
+  },
+  {
+    content:"So many books, so little time.",
+    from: "Frank Zappa"
+  },
+  {
+    content:"Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+    from: "Bernard M. Baruch"
+  },
+  {
+    content:"A room without books is like a body without a soul.",
+    from: "Marcus Tullius Cicero"
+  },
+  {
+    content:"You've gotta dance like there's nobody watching, \n Love like you'll never be hurt, \n Sing like there's nobody listening, \nAnd live like it's heaven on earth.",
+    from: "William W. Purkey"
+  },
+  {
+    content:"You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+    from: "Dr. Seuss"
+  },
+  {
+    content:"You only live once, but if you do it right, once is enough.",
+    from: "Mae West"
+  },
+  {
+    content:"Be the change that you wish to see in the world.",
+    from: "Mahatma Gandhi"
+  },
+  {
+    content:"In three words I can sum up everything I've learned about life: it goes on.",
+    from: "Robert Frost"
+  }
+]
